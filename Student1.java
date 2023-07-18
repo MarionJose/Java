86b0e57e79efceb1cd354e9329ae341caee338c5
Marion <marionjosekoottala@gmail.com>
2023-07-18 21:51:09 +0530

    Paraexample

---
+++ Student1.java
@@ -0,0 +1,16 @@
+public class Student1 {
+   int rolno;
+   String name;
+   public void Student1(int i) { 
+      rolno = i;
+     
+   }
+   void display() {
+      System.out.println(rolno is :);
+   }
+   public static void main(String args[]) {
+      Student s1 = new Student1(int 1);
+      s1.display();
+     
+   }
+}
